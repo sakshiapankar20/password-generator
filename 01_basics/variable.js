0e109ed6a1f7f0febe3d14c5bd6aa906f7c7ed6f
sakshiapankar20 <142079744+sakshiapankar20@users.noreply.github.com>
2024-06-21 12:52:52 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 23249
+let accountEmail="sakhi@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+// accountId=34353 //not allowed
+
+accountEmail="anc@gmail.com"
+accountPassword='39489'
+accountCity='pune'
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
