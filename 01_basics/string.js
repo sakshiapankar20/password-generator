fc6a7a15af0c1a6ea54225953ac1a023765f02e2
sakshiapankar20 <142079744+sakshiapankar20@users.noreply.github.com>
2024-06-21 16:51:30 +0000

    container config done

---
+++ 01_basics/string.js
@@ -0,0 +1,25 @@
+const user="sakshi"
+const value=12;
+console.log(user+value+' alive');
+console.log(`user name is ${user} and it's value is ${value}`)
+
+const userName=new String("  Hello-world  ")
+console.log(userName);
+console.log(userName[3]);
+console.log(userName.length);
+console.log(userName.toLowerCase());
+console.log(userName.toUpperCase());
+console.log(userName.charAt(4))
+console.log(userName.charCodeAt(0))
+console.log(userName.indexOf('w'))
+console.log(userName.includes('world'))
+console.log(userName.slice(-4,10))
+console.log(userName.replace('world','sakshi'))
+console.log(userName.substring(1,5));
+console.log(userName.trim())
+console.log(userName.big())
+console.log(userName.endsWith(' '))
+
+
+
+
