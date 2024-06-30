04b22778f0095298dde28c4890e8dbbc8b51c2f3
sakshiapankar20 <142079744+sakshiapankar20@users.noreply.github.com>
2024-06-30 17:21:22 +0000

    done

---
+++ 01_basics/date.js
@@ -0,0 +1,14 @@
+let myDate=new Date(2024,5,30,22,48)
+console.log(myDate)
+console.log(myDate.getDay()+1)
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.getFullYear())
+
+
+
+console.log(myDate.toLocaleTimeString("en-IN"))
