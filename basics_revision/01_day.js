4db1bf9aa3143a0eca9449ce2a6f82c47eca3f43
sakshiapankar20 <142079744+sakshiapankar20@users.noreply.github.com>
2025-07-15 07:14:33 +0000

    done

--- basics_revision/01_day.js
+++ basics_revision/01_day.js
@@ -1,1 +1,34 @@
+/*
 console.log("hello javascript");
+
+const accountId=294802;
+
+let accountEmail="sakshi@gmail.com";
+accountEmail="da@gmail.com";
+console.log(accountEmail);
+let accountState;
+
+var accountPassword="dlsj3r";
+// please don't use var because of block scope 
+// and functional scope
+
+accountPassword="38739";
+console.log(accountPassword);
+
+accountCity="Mumbai";
+
+console.log(accountCity);
+
+// accountId=39830;// we cannot reassign value to const variable
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
+*/
+
+
+
+//variables
+
+// alert("hello"); we are using nodejs, not browser
+
+console.log("sakshi")
