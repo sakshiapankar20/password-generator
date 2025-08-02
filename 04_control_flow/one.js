f666c04f23381aa7d2820e1b4e91689d40ab0bb2
Sakshi Ganesh Apankar <142079744+sakshiapankar20@users.noreply.github.com>
2025-08-02 18:38:05 +0000

    control flow done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,76 @@
+//control flow 
+
+//if
+
+// comparison opertors-> <,>,<=,>=,==,!=,===,!==
+
+// atrict equality and inequality operator===,!==->checks value as well as datatypes
+
+// let balance=1000;
+
+// if(balance<500){
+//     console.log("less than 500");
+// }
+// else if(balance<750){
+//     console.log("less than 750");
+// }
+// else if(balance < 900){
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+
+// const userLoggedIn=true;
+// const debitCard=true;
+// const loggedInFromGoogl=false;
+// const loggedInFromEmail=true;
+
+
+
+// if(userLoggedIn==true && debitCard==true){
+//     console.log("allow to buy course")
+// }
+
+
+// if(loggedInFromEmail==true || loggedInFromGoogl==true){
+//     console.log("user logged In");
+// }
+
+
+
+
+// switch case
+
+// const month=7;
+
+// switch (month) {
+//     case 1:console.log("january")
+        
+//         break;
+//     case 2:console.log("February")
+        
+//         break;
+//     case 3:console.log("March")
+        
+//         break;
+//     case 4:console.log("april")
+        
+//         break;
+//     case 5:console.log("may")
+        
+//         break;
+//     case 6:console.log("june")
+        
+//         break;
+//     case 7:console.log("july");
+        
+//         // break;
+//     case 8:console.log("august");
+        
+//         // break;
+
+//     default:console.log("month after august");
+//         break;
+// }
