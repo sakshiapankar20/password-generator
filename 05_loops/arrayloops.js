bc792c31f9894f9efe775199a4259b08b80f25fa
Sakshi Ganesh Apankar <142079744+sakshiapankar20@users.noreply.github.com>
2025-08-04 07:51:05 +0000

    arrayloops

---
+++ 05_loops/arrayloops.js
@@ -0,0 +1,168 @@
+// for of loop
+
+// let myarr=["batman","spiderman","ironman"];
+// const arr=[1,2,3,4,5];
+
+
+// for (const i of myarr) {
+//     console.log(i);
+    
+// }
+
+// const greetings="hello world";
+// for (const j of greetings) {
+//     if(j==" "){
+//         continue;
+//     }
+//     console.log(j);
+// }
+
+
+//maps
+
+// const map=new Map();
+// map.set('IN',"INDIA");
+// map.set('USA',"UNITED STATES OF AMERICA");
+// map.set('Fr',"France");
+
+// console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key,": ",value);
+    
+// }
+
+
+// const myObj={
+//     game1:'NFS',
+//     game2:'spiderman'
+// }
+
+// for (const element of myobj) {
+//     console.log(element);
+    
+    
+// }
+
+// for (const [key,value] of myobj) {
+//     console.log(element);
+//     console.log(key,": ",value);
+    
+    
+// }
+
+
+//forof loop is not working for objects
+
+
+
+// forin loop
+
+// for objects
+
+// const myObjects={
+//     js:'javascript',
+//     cpp:'C++',
+//     rb:'ruby',
+//     swift:'swift by apple'
+// }
+
+// for (const key in myObjects) {
+//     console.log(key);
+ 
+// }
+// for (const key in myObjects) {
+//     console.log("the value of keys are :"+myObjects[key]);
+ 
+// }
+
+
+// for array
+
+// const programming=['cpp','python'];
+// for(const key in programming){
+//     console.log(key);
+// }
+
+// for(const key in programming){
+//     console.log(programming[key]);
+    
+// }
+
+
+
+
+// const map=new Map();
+// map.set('IN',"INDIA");
+// map.set('USA',"UNITED STATES OF AMERICA");
+// map.set('Fr',"France");
+
+
+// for(const key in map){
+//     console.log(key);
+    
+// }
+
+
+//for in loop is not working on map
+
+
+
+////foreach loop
+
+const arr=[1,2,3,4,5];
+
+arr.forEach((print));
+
+function print(item){
+    console.log(item);
+}
+
+// arr.forEach((i)=>{
+//     console.log(i);
+    
+// })
+
+
+// arr.forEach(function (i){
+//     console.log(i);
+// })
+
+
+// arr.forEach((value,index,array)=>{
+//     console.log(`value: ${value}, index: ${index}, array: ${array}`);
+    
+// })
+
+
+// arr.forEach(element => {
+//     console.log(element)
+// });
+
+
+// object inside array
+
+// const mycoding=[
+//     {
+//         languageName:"javaScript",
+//         languageFileName:"js"
+//     },
+//     {
+//         languageName:"java",
+//         languageFileName:"java"
+//     },
+//     {
+//         languageName:"python",
+//         languageFileName:"py"
+//     }
+// ]
+
+// mycoding.forEach((item)=>{
+//     console.log(item)
+// })
+// mycoding.forEach((item)=>{
+//     console.log(item.languageName)
+// })
+// mycoding.forEach((item)=>{
+//     console.log(item.languageFileName)
+// })
