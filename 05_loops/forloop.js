9634a6beea834149c01523292a109e2a2ee51d73
Sakshi Ganesh Apankar <142079744+sakshiapankar20@users.noreply.github.com>
2025-08-04 06:12:18 +0000

    loops done

---
+++ 05_loops/forloop.js
@@ -0,0 +1,76 @@
+// for(let i=0; i< 10 ;i++){
+//     console.log(i+1);
+    
+// }
+// let patt="";
+// for(let i=0; i< 3; i++){
+//     for(let j=0 ;j<=i; j++){
+//         patt+="* ";
+        
+//     }
+   
+//     patt+="\n";
+// }
+
+// console.log(patt);
+// let patt1="";
+// for(let i=0; i< 3; i++){
+//     for(let j=0 ;j<=i; j++){
+//         patt1+=(i+1);
+        
+//     }
+   
+//     patt1+="\n";
+// }
+
+// console.log(patt1);
+
+
+// let myArray=["sakshi","kabeer"];
+// for(let i=0; i<myArray.length; i++){
+//     const element=myArray[i];
+//     console.log(element);
+// }
+
+
+// console.log("continue");
+
+
+// for(let i=1; i<=20; i++){
+//     if(i%5==0){
+//         console.log("skip");
+//         continue;
+//     }
+//     console.log(`the value of i is ${i}`);
+    
+// }
+
+
+
+
+// console.log("\nbreak");
+
+// for(let i=1; i<=20; i++){
+//     if(i%5==0){
+//         console.log("out");
+//         break;
+//     }
+//     console.log(`the value of i is ${i}`);
+    
+// }
+
+
+// let i=1;
+// while (i<=5) {
+//     console.log(`the value of i is ${i}`);
+    
+//     i++;
+// }
+
+
+let i=1;
+do{
+    console.log(`the value of i is ${i}`);
+    
+    i++;
+}while (i<0) ;
