4cb6480e908b2e8385edc076081c228d1ff413ee
Sakshi Ganesh Apankar <142079744+sakshiapankar20@users.noreply.github.com>
2025-08-04 04:45:20 +0000

    done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -74,3 +74,87 @@
 //     default:console.log("month after august");
 //         break;
 // }
+
+
+
+// const userEmail="abc@gmail.com";->console.log("got user email");
+// const userEmail="";->console.log("Don't have user email");
+// const userEmail=" ";-> console.log("got user email");
+    
+// const userEmail=[];-> console.log("got user email");
+
+
+
+// if(userEmail){
+//     console.log("got user email");
+    
+// }
+// else{
+//     console.log("Don't have user email");
+    
+// }
+
+
+// falsy values
+
+// false,0,-0,BigInt 0n,"",null,undefined,Nan
+
+//truthy values
+// "0","false"," ",[],{},function(){}
+
+
+// if array is empty
+// const userEmail=[];
+
+// if(userEmail.length===0){
+//     console.log("Array is empty");
+    
+// }
+
+// // if object is empty how to get to know
+// const emptyObj={};
+// if(Object.keys(emptyObj).length===0){
+//     console.log("object is empty");
+    
+// }
+
+
+// useful comparisons
+// false==0;
+// false=="";
+// 0=="";
+
+// // console.log(false==0,false=="",""==0);
+// console.table([false==0,false=="",""==0])
+
+
+// nullish coalescing operator (??): null undefined
+
+let val1;
+// val1=5 ?? 10;
+// console.log(val1);//5
+
+// val1=null ?? 10;
+// console.log(val1);//10
+
+// val1=undefined ?? 15;
+// console.log(val1);//15
+
+
+// val1=null??10??20;
+// console.log(val1);
+
+
+//ternary operator
+
+// condition ? true : false
+
+const iceTeaPrice=100;
+
+iceTeaPrice<=70 ? console.log("we can buy ice-tea")
+ : console.log("we can't buy ice-tea")
+ ;
+
+// const result= iceTeaPrice<=70 ? "we can buy ice-tea" : "we can't buy ice-tea";
+
+// console.log(result)
