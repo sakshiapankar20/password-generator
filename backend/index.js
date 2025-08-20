41a20a55698204402e7bd5b85f41c51e2c4b024c
Sakshi Ganesh Apankar <142079744+sakshiapankar20@users.noreply.github.com>
2025-08-20 17:18:52 +0000

    backend

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express=require('express');
+
+const app=express();
+// const port=4000;
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+});
+
+app.get("/twitter",(req,res)=>{
+    res.send("hellotwitter");
+})
+
+app.get("/login",(req,res)=>{
+    res.send("this is login page");
+})
+
+app.get("/youtube",(req,res)=>{
+    res.send("<h2>sakshi apankar</h2>");
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`the app is running on port ${process.env.PORT}`);
+});
