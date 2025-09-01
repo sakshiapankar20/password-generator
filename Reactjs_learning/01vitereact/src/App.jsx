98d4ace9d1761c3ced557036f8ee9aba883834e0
Sakshi Ganesh Apankar <142079744+sakshiapankar20@users.noreply.github.com>
2025-09-01 06:17:13 +0000

    creating react project done

---
+++ Reactjs_learning/01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  
+
+  return (
+     <h1>Learning React with vite | Sakshi Apankar </h1>
+  )
+}
+
+export default App
---
+++ Reactjs_learning/1basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Learning React | Sakshi Apankar</h1>
+  );
+}
+
+export default App;
