83728fa026519ff2736b1638471d6dd2b527b203
Sakshi Ganesh Apankar <142079744+sakshiapankar20@users.noreply.github.com>
2025-09-03 07:08:03 +0000

    done

--- Reactjs_learning/01vitereact/src/main.jsx
+++ Reactjs_learning/01vitereact/src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client"
+import App from "./App"
 
-import App from './App.jsx'
+const reactEle=React.createElement(
+  'a',
+  {
+    href:'https://google.com',
+    target:'_blank'
+  },
+  'click to visit google'
+)
 
+function Myapp(){
+  return(
+    <div>
+      <h3>hello this is day 3 of learning reactjs</h3>
+    </div>
+  )
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {reactEle}
+    <App/>
+    {Myapp()} 
+    
+    {/* this is the way to declare functions inside strict mode */}
+  </StrictMode>
+
 )
