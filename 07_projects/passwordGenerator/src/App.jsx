9f5f2b2b4306531ed06f53622a34eb8533179051
Sakshi Ganesh Apankar <142079744+sakshiapankar20@users.noreply.github.com>
2025-09-10 19:00:22 +0000

    done

--- 07_projects/passwordGenerator/src/App.jsx
+++ 07_projects/passwordGenerator/src/App.jsx
@@ -1,16 +1,50 @@
-
-
-
+import { useState, useCallback } from "react";
 
 function App() {
-  
+  const [length, setLength] = useState(8);
+
+  const [numberAllow, setNumberAllow] = useState(false);
+
+  const [charAllow, setCharAllow] = useState(false);
+
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllow) {
+      str += "0123456789";
+    }
+
+    if (charAllow) {
+      str += "!@#$%^&*()_+-={}[]`~";
+    }
+
+    for (let i = 1; i <= Array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllow, charAllow, setPassword]);
 
   return (
     <>
-    <h1 className='text-4xl text-center'>Password generator</h1>
-       
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800">
+        <h1 className="text-4xl text-center text-white">Password generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
